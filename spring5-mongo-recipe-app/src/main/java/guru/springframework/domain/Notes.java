a41a8925cf17dd1e842213b4c773d8ecb94c88c2
mycodeinpeace <me@codeinpeace.com>
2022-08-23 11:50:11 +0300

    spring5-mongo-recipe-app - Refactoring Data Model for MongoDB

--- spring5-mongo-recipe-app/src/main/java/guru/springframework/domain/Notes.java
+++ spring5-mongo-recipe-app/src/main/java/guru/springframework/domain/Notes.java
@@ -2,14 +2,16 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 
 @Getter
 @Setter
 public class Notes {
 
+    @Id
     private String id;
-    private Recipe recipe;
+    //private Recipe recipe;
     private String recipeNotes;
 
 }
