c005fd0f8922d96a1d568b90302f9446507532c6
mycodeinpeace <me@codeinpeace.com>
2022-08-25 13:25:28 +0300

    spring5-reactive-mongo-recipe-app - Convert to Reactive Repositories

---
+++ spring5-reactive-mongo-recipe-app/src/test/java/guru/springframework/repositories/reactive/CategoryReactiveRepositoryTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.Category;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class CategoryReactiveRepositoryTest extends TestCase {
+
+    @Autowired
+    CategoryReactiveRepository categoryReactiveRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryReactiveRepository.deleteAll().block();
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        Category category = new Category();
+        category.setDescription("Foo");
+
+        categoryReactiveRepository.save(category).block();
+
+        Long count = categoryReactiveRepository.count().block();
+
+        assertEquals(Long.valueOf(1L), count);
+    }
+
+    @Test
+    public void testFindByDescription() {
+        Category category = new Category();
+        category.setDescription("foo");
+
+        categoryReactiveRepository.save(category).block();
+
+        Category fetchedCategory = categoryReactiveRepository.findByDescription("foo").block();
+
+        assertNotNull(fetchedCategory.getId());
+    }
+}
