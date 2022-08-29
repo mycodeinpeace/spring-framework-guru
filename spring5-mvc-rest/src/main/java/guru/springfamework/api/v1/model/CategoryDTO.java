1cb945f41f1c29afeb99ab15d59cea0812783c19
mycodeinpeace <me@codeinpeace.com>
2022-08-29 22:38:59 +0300

    spring5-mvc-rest - MapStruct IntelliJ Plugin

--- spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -2,8 +2,17 @@ package guru.springfamework.api.v1.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class CategoryDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
